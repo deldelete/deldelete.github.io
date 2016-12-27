004d4ac1f0d593bafd8eca6d89ef6eeb217b8b26
Dor Rondel <dor.rondel@gmail.com>
2016-12-26 23:25:16 -0500

    added temp site

---
+++ src/scripts/index.js
@@ -0,0 +1,6 @@
+$(() => {
+  $(".button-collapse").sideNav();
+  $('.carousel.carousel-slider').carousel({full_width: true, indicators: true});
+  $(".dropdown-button").dropdown({hover: true});
+  $('.parallax').parallax();
+});
