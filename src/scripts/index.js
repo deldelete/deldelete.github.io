5413b56e7cddfad7f3b4ff89c61719aec3d63e07
Dor Rondel <dor.rondel@gmail.com>
2017-01-18 01:05:30 -0500

    fixed iphone issue, added translater, fix color palette

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-$(() => {
+$(function() { /* Changed from anon function for safari compatibility */
   $(".button-collapse").sideNav();
   $(".carousel-slider").carousel({full_width: true, indicators: true});
   $(".dropdown-button").dropdown({hover: true});
