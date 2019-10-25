93798b20f437848f66ada41562da3ff5c64b7b12
zlatkoflashd <55800969+zlatkoflashd@users.noreply.github.com>
2019-10-25 21:50:26 +0200

    Contact Form and Phone Number in header

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,32 @@
 $(function() { /* Changed from anon function for safari compatibility */
-  $(".button-collapse").sideNav();
-  $(".carousel-slider").carousel({full_width: true, indicators: true});
-  $(".dropdown-button").dropdown({hover: true});
-  $(".parallax").parallax();
+  	$(".button-collapse").sideNav();
+  	$(".carousel-slider").carousel({full_width: true, indicators: true});
+  	$(".dropdown-button").dropdown({hover: true});
+  	$(".parallax").parallax();
+
+  	if($('.slider-contact-form form').length==1){
+	  	$('.slider-contact-form form').validate({
+		    ignore:'',
+		    rules:{
+		      	'your-name'  :'required',
+		      	'your-email' :'required',
+		      	'your-phone' :'required',
+		      	'your-message' :'required'
+		    },
+		    errorPlacement: function(error, element) {
+		      	var placement = $(element).data('error');
+		      	if (placement) {
+		        	$(placement).append(error)
+		      	} else {
+		        	error.insertAfter(element);
+		      	}
+		    }
+	  	});
+	  	$(document).on('click', '.slider-contact-form .submit-button', function(e){
+			if($('.slider-contact-form form').valid()){
+				$('.slider-contact-form form').submit();
+			}
+			return false;
+		});
+	}
 });
